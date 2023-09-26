dbdc863e9a2767e38e8337444dc5f6acf427fb4a
safrulmunajat <safrulmunajat@gmail.com>
2023-09-26 16:03:19 +0700

    min height total customer doughnut chart

--- components/home/section/home-section-title.js
+++ components/home/section/home-section-title.js
@@ -1,17 +1,30 @@
+import { Button, Col, Container, Row } from "react-bootstrap";
 import styles from "./home-section-title.module.css";
+
 const HomeSectionTitle = ({ title, subtitle }) => {
   return (
-    <div className={styles.header}>
-      <div className={styles.title}>
-        <div className={styles.newArrivalsProduk}>{title}</div>
-        <div className={styles.checkOutWhats}>{subtitle}</div>
-      </div>
-      <div className={styles.componentnavigationbutton}>
-        <div className={styles.secondaryMedium}>
-          Shop new arrival Belanja produk baru
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col xs="6" md="6" className="text-left">
+          <div className={styles.title}>
+            <div className={styles.newArrivalsProduk}>{title}</div>
+            <div className={styles.checkOutWhats}>{subtitle}</div>
+          </div>
+        </Col>
+        <Col xs="6" md="6" className="text-right">
+          <Button variant="outline-primary">Shop new arrival Belanja produk baru</Button>{' '}
+          {/* <div className={styles.componentnavigationbutton}>
+            <div className={styles.secondaryMedium}>
+              
+            </div>
+          </div> */}
+        </Col>
+      </Row>
+      {/* <div className={styles.header}>
+
+
+      </div> */}
+    </Container>
   );
 };
 
